e23cb615c1abba3f5d14241246f29507cb571fbe
Parbat Limbu <50946258+Habray@users.noreply.github.com>
2020-11-13 00:39:23 +0545

    I am rich app

---
+++ android/app/src/main/kotlin/parbatlimbu/com/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package parbatlimbu.com.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("I am Rich"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[600],
+        body: Center(child: Image(
+          image: AssetImage(
+              'assets/diamond.png'),
+        ),),
+      ),
+    ),
+  );
+}
